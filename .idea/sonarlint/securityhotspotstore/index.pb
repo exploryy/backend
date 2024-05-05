e1cbc3ad9f38036bdfd42e1570b5d003ccb31632
Gordey Dovydenko <inostaziss@gmail.com>
2024-05-05 18:50:47 +0700

    added db structure

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,22 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+?
+db/build.gradle,d\9\d9c31db3379579d5a92445e95f1ffbdb069bc8ff
+I
+gradle/libs.versions.toml,d\7\d7595319a0658be6f1ed500ad590d42e4b28bc4f
+=
+db/Dockerfile,f\4\f458ef942ebe2348b78e3b9854bbd59a08a489f5
+?
+db/liquibase.sh,9\7\97c99db8b7070307db739b220d65bdf7f03935c8
+:
+
+Dockerfile,6\6\6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+B
+docker-compose.yml,3\5\35b8c13cf2eb2a194eada000eb310d65aed53b2a
+4
+.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+O
+db/changelog/root-changelog.yml,c\e\ceea9c28cba14bdf524f314aa478bafc8683667f
+S
+#db/changelog/ddl/initial_schema.sql,0\9\091ef469737ee6d507cfdacbdb38cea9cd3c367b
---
+++ db/changelog/ddl/initial_schema.sql
@@ -0,0 +1,156 @@
+-- liquibase formatted sql
+
+-- changeset gordey_dovydenko:1
+
+create sequence s_quest_id start with 1 increment by 1;
+
+create table quest (
+    quest_id bigint default nextval('s_quest_id'),
+    name varchar(60) not null,
+    description varchar(255) not null,
+    difficulty_type varchar(20) check ( difficulty_type in ('EASY', 'MEDIUM', 'HARD') ) not null,
+    quest_type varchar(20) check ( quest_type in ('POINT_TO_POINT', 'DISTANCE') ) not null,
+    transport_type varchar(20) check ( transport_type in ('WALK', 'BICYCLE') ) not null,
+    primary key (quest_id)
+);
+
+-- rollback DROP TABLE quest;
+-- rollback DROP SEQUENCE s_quest_id;
+
+-- changeset gordey_dovydenko:2
+
+create sequence s_route_id start with 1 increment by 1;
+
+create table route (
+    route_id bigint default nextval('s_route_id'),
+    distance double precision not null,
+    primary key (route_id)
+);
+
+-- rollback DROP TABLE route;
+-- rollback DROP SEQUENCE s_route_id;
+
+-- changeset gordey_dovydenko:3
+
+create sequence s_pass_quest_id start with 1 increment by 1;
+
+create table pass_quest (
+    pass_quest_id bigint default nextval('s_pass_quest_id'),
+    quest_id bigint not null,
+    client_id bigint not null,
+    start_time timestamp not null,
+    end_time timestamp not null,
+    route_id bigint not null,
+    transport_type varchar(20) check ( transport_type in ('WALK', 'BICYCLE') ) not null,
+    primary key (pass_quest_id),
+    foreign key (quest_id) references quest(quest_id),
+    foreign key (route_id) references route(route_id)
+);
+
+-- rollback DROP TABLE pass_quest;
+-- rollback DROP SEQUENCE s_quest_point_id;
+
+-- changeset gordey_dovydenko:4
+
+create sequence s_quest_review_id start with 1 increment by 1;
+
+create table quest_review (
+    quest_review_id bigint default nextval('s_quest_review_id'),
+    score integer check ( score >= 1 and score <= 5 ) not null,
+    message varchar(255) not null,
+    client_id bigint not null,
+    quest_id bigint not null,
+    primary key (quest_review_id),
+    foreign key (quest_id) references quest(quest_id)
+);
+
+-- rollback DROP TABLE quest_review;
+-- rollback DROP SEQUENCE s_quest_review_id;
+
+-- changeset gordey_dovydenko:5
+
+create sequence s_review_photo_id start with 1 increment by 1;
+
+create table review_photo (
+    photo_id bigint default nextval('s_review_photo_id'),
+    review_id bigint not null,
+    primary key (photo_id),
+    foreign key (review_id) references quest_review(quest_review_id)
+);
+
+-- rollback DROP TABLE review_photo;
+-- rollback DROP SEQUENCE s_review_photo_id;
+
+-- changeset gordey_dovydenko:6
+
+create sequence s_quest_photo_id start with 1 increment by 1;
+
+create table quest_photo (
+    photo_id bigint default nextval('s_quest_photo_id'),
+    quest_id bigint not null,
+    primary key (photo_id),
+    foreign key (quest_id) references quest(quest_id)
+);
+
+-- rollback DROP TABLE quest_photo;
+-- rollback DROP SEQUENCE s_quest_photo_id;
+
+-- changeset gordey_dovydenko:7
+
+create table point_to_point_quest (
+    quest_id bigint not null,
+    route_id bigint not null,
+    primary key (quest_id),
+    foreign key (quest_id) references quest(quest_id),
+    foreign key (route_id) references route(route_id)
+);
+
+-- rollback DROP TABLE point_to_point_quest;
+
+-- changeset gordey_dovydenko:8
+
+create table distance_quest (
+    quest_id bigint not null,
+    route_distance double precision not null,
+    primary key (quest_id),
+    foreign key (quest_id) references quest(quest_id)
+);
+
+-- rollback DROP TABLE distance_quest;
+
+-- changeset gordey_dovydenko:9
+
+create table point (
+    latitude varchar(60) not null,
+    longitude varchar(60) not null,
+    previous_latitude varchar(60),
+    previous_longitude varchar(60),
+    primary key (latitude, longitude),
+    foreign key (previous_latitude, previous_longitude) references point(previous_latitude, previous_longitude)
+);
+
+-- rollback DROP TABLE point;
+
+-- changeset gordey_dovydenko:10
+
+create sequence s_achievement_id start with 1 increment by 1;
+
+create table achievement (
+    achievement_id bigint default nextval('s_achievement_id'),
+    name varchar(60) not null,
+    description varchar(255) not null,
+    primary key (achievement_id)
+);
+
+-- rollback DROP TABLE achievement;
+
+-- changeset gordey_dovydenko:11
+
+create table client_achievement (
+    client_id bigint not null,
+    achievement_id bigint not null,
+    primary key (client_id, achievement_id),
+    foreign key (achievement_id) references achievement(achievement_id)
+);
+
+-- rollback DROP TABLE client_achievement;
