b10c9e60e3070db895097907f292c630faeecdd4
T9404 <ghubman1@gmail.com>
2024-06-21 18:03:16 +0700

    update

--- src/main/java/com/hits/open/world/rest/controller/buff/BuffController.java
+++ src/main/java/com/hits/open/world/rest/controller/buff/BuffController.java
@@ -12,11 +12,19 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/buff")
+@RequestMapping("/buffs")
 @SecurityRequirement(name = "oauth2")
-@Tag(name = "Buff")
+@Tag(name = "Buffs")
 public class BuffController {
     private final BuffService buffService;
+
+    @PostMapping("/apply")
+    public void applyBuff(@RequestParam("buffId") Long buffId,
+                          JwtAuthenticationToken token
+    ) {
+        var userId = token.getTokenAttributes().get("sub").toString();
+        buffService.applyBuff(buffId, userId);
+    }
 
     @GetMapping("/my")
     public List<BuffDto> getMyBuffs(JwtAuthenticationToken token) {
@@ -24,15 +32,9 @@ public class BuffController {
         return buffService.getMyBuffs(userId);
     }
 
-    @GetMapping("/all")
-    public List<BuffDto> getAll(@RequestParam(required = false) Integer level) {
+    @GetMapping("/available")
+    public List<BuffDto> getAvailableBuffs(@RequestParam(required = false) Integer level) {
         return buffService.getAll(level);
     }
 
-    @PostMapping("/apply")
-    public BuffDto applyBuff(@RequestParam Long buffId,
-                                   JwtAuthenticationToken token) {
-        var userId = token.getTokenAttributes().get("sub").toString();
-        return buffService.applyBuff(buffId, userId);
-    }
 }
